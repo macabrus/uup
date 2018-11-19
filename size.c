7d1d7257016eadc5ac65c4eb61a97776a9e9b7a5
macabrus <bernardcrnkovicc@gmail.com>
2018-11-19 20:32:59 +0100

    .

--- size.c
+++ size.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 
 int main(){
-   int a = 32;
-   int b = 3;
-   int c = ~b + 1;
-   printf("%d\n",0x7FFFFFFF);
+   short short a = 100;
+   printf("%d\n",sizeof(short short));
 }
